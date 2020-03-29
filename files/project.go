8cff2b650258f6a1d6219686b2ed57d35b3b9572
delai21 <k.gysin.kg@gmail.com>
2020-03-29 12:34:11 +0200

    read local project config

--- files/project.go
+++ files/project.go
@@ -1,17 +1,86 @@
 package files
 
+import (
+	yamld "gopkg.in/yaml.v2"
+	"log"
+)
+
+const ConfigFileSuffix = "config.yaml"
+
 type OpenshiftProject struct {
 	Name       string
 	ConfigFile string
+	local      bool
 	Items      []OpenshiftItem
 }
 
 type OpenshiftItem struct {
-	Name string
-	File string
-	Data string
+	Name    string
+	File    string
+	Data    string
+	RawData []byte
 }
 
-func (op OpenshiftProject) loadItemsData() {
+//func NewFromFolder(folder string) (*OpenshiftProject, error) {
+//	fileDatas, err := ReadAllFilesInFolder(folder)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	var folderItems []OpenshiftItem
+//	for file, data := range fileDatas {
+//		if strings.HasSuffix(file, "config.yaml") {
+//
+//		}
+//		item := NewOpenshiftItemFromFile("", file, data)
+//		folderItems = append(folderItems, item)
+//	}
+//
+//	project := &OpenshiftProject{
+//		Name:       "",
+//		ConfigFile: "",
+//		local:      true,
+//		Items:      folderItems,
+//	}
+//
+//	return project, nil
+//}
 
+func NewProjectFromConfig(folder string) (*OpenshiftProject, error) {
+	configData, err := ReadConfigFile(folder)
+	if err != nil {
+		return nil, err
+	}
+	var newProject *OpenshiftProject
+	err = yamld.Unmarshal(configData, newProject)
+	if err != nil {
+		return nil, err
+	}
+	return newProject, nil
+}
+
+func NewOpenshiftItemFromFile(name string, file string, rawdata []byte) OpenshiftItem {
+	return OpenshiftItem{
+		Name:    name,
+		File:    file,
+		RawData: rawdata,
+	}
+}
+
+func NewOpenshiftItem(name string, file string, data string) OpenshiftItem {
+	return OpenshiftItem{
+		Name: name,
+		File: file,
+		Data: data,
+	}
+}
+
+func (op *OpenshiftProject) LoadProjectFiles() {
+	for _, item := range op.Items {
+		fileData, err := ReadFile(item.File)
+		if err != nil {
+			log.Fatalf("could not read file [%]\n", item)
+		}
+		item.RawData = fileData
+	}
 }
