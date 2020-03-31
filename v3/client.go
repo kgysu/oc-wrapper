abdb3be752047d3e834db79c21101c5b6a0d99ef
delai21 <k.gysin.kg@gmail.com>
2020-03-31 20:12:42 +0200

    fix client api

--- v3/client.go
+++ v3/client.go
@@ -5,114 +5,115 @@ import (
 	v13 "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1"
 	v12 "k8s.io/client-go/kubernetes/typed/core/v1"
 	v14 "k8s.io/client-go/kubernetes/typed/rbac/v1"
+	"k8s.io/client-go/rest"
 )
 
-func GetDeploymentConfigsInterface(namespace string, localConfig bool) (v1.DeploymentConfigInterface, error) {
-	appsClient, err := GetAppsV1Client(localConfig)
+func GetDeploymentConfigsInterface(namespace string, restConf *rest.Config) (v1.DeploymentConfigInterface, error) {
+	appsClient, err := GetAppsV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return appsClient.DeploymentConfigs(namespace), nil
 }
 
-func GetServicesInterface(namespace string, localConfig bool) (v12.ServiceInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetServicesInterface(namespace string, restConf *rest.Config) (v12.ServiceInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.Services(namespace), nil
 }
 
-func GetConfigMapsInterface(namespace string, localConfig bool) (v12.ConfigMapInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetConfigMapsInterface(namespace string, restConf *rest.Config) (v12.ConfigMapInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.ConfigMaps(namespace), nil
 }
 
-func GetRoutesInterface(namespace string, localConfig bool) (v13.RouteInterface, error) {
-	routeClient, err := GetRouteV1Client(localConfig)
+func GetRoutesInterface(namespace string, restConf *rest.Config) (v13.RouteInterface, error) {
+	routeClient, err := GetRouteV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return routeClient.Routes(namespace), nil
 }
 
-func GetRolesInterface(namespace string, localConfig bool) (v14.RoleInterface, error) {
-	rbacClient, err := GetRbacV1Client(localConfig)
+func GetRolesInterface(namespace string, restConf *rest.Config) (v14.RoleInterface, error) {
+	rbacClient, err := GetRbacV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return rbacClient.Roles(namespace), nil
 }
-func GetRoleBindingsInterface(namespace string, localConfig bool) (v14.RoleBindingInterface, error) {
-	rbacClient, err := GetRbacV1Client(localConfig)
+func GetRoleBindingsInterface(namespace string, restConf *rest.Config) (v14.RoleBindingInterface, error) {
+	rbacClient, err := GetRbacV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return rbacClient.RoleBindings(namespace), nil
 }
 
-func GetEventsInterface(namespace string, localConfig bool) (v12.EventInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetEventsInterface(namespace string, restConf *rest.Config) (v12.EventInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.Events(namespace), nil
 }
-func GetReplicationControllersInterface(namespace string, localConfig bool) (v12.ReplicationControllerInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetReplicationControllersInterface(namespace string, restConf *rest.Config) (v12.ReplicationControllerInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.ReplicationControllers(namespace), nil
 }
-func GetEndpointsInterface(namespace string, localConfig bool) (v12.EndpointsInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetEndpointsInterface(namespace string, restConf *rest.Config) (v12.EndpointsInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.Endpoints(namespace), nil
 }
-func GetSecretsInterface(namespace string, localConfig bool) (v12.SecretInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetSecretsInterface(namespace string, restConf *rest.Config) (v12.SecretInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.Secrets(namespace), nil
 }
-func GetPersistentVolumeClaimsInterface(namespace string, localConfig bool) (v12.PersistentVolumeClaimInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetPersistentVolumeClaimsInterface(namespace string, restConf *rest.Config) (v12.PersistentVolumeClaimInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.PersistentVolumeClaims(namespace), nil
 }
-func GetPodsInterface(namespace string, localConfig bool) (v12.PodInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetPodsInterface(namespace string, restConf *rest.Config) (v12.PodInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.Pods(namespace), nil
 }
-func GetServiceAccountsInterface(namespace string, localConfig bool) (v12.ServiceAccountInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetServiceAccountsInterface(namespace string, restConf *rest.Config) (v12.ServiceAccountInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.ServiceAccounts(namespace), nil
 }
 
-func GetResourceQuotasInterface(namespace string, localConfig bool) (v12.ResourceQuotaInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetResourceQuotasInterface(namespace string, restConf *rest.Config) (v12.ResourceQuotaInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return coreClient.ResourceQuotas(namespace), nil
 }
-func GetLimitRangesInterface(namespace string, localConfig bool) (v12.LimitRangeInterface, error) {
-	coreClient, err := GetCoreV1Client(localConfig)
+func GetLimitRangesInterface(namespace string, restConf *rest.Config) (v12.LimitRangeInterface, error) {
+	coreClient, err := GetCoreV1Client(restConf)
 	if err != nil {
 		return nil, err
 	}
--- v3/clientUtil.go
+++ v3/clientUtil.go
@@ -1,64 +1,84 @@
 package v3
 
 import (
+	"fmt"
 	appsv1client "github.com/openshift/client-go/apps/clientset/versioned/typed/apps/v1"
 	routev1client "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1"
 	corev1client "k8s.io/client-go/kubernetes/typed/core/v1"
 	rbacv1client "k8s.io/client-go/kubernetes/typed/rbac/v1"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
+	"os"
 )
 
-func GetAppsV1Client(fromLocal bool) (*appsv1client.AppsV1Client, error) {
-	restConfig, err := GetConfig(fromLocal)
-	if err != nil {
-		return nil, err
-	}
-	client, err := appsv1client.NewForConfig(restConfig)
+func GetAppsV1Client(restConf *rest.Config) (*appsv1client.AppsV1Client, error) {
+	client, err := appsv1client.NewForConfig(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return client, nil
 }
 
-func GetCoreV1Client(fromLocal bool) (*corev1client.CoreV1Client, error) {
-	restConfig, err := GetConfig(fromLocal)
-	if err != nil {
-		return nil, err
-	}
-	client, err := corev1client.NewForConfig(restConfig)
+func GetCoreV1Client(restConf *rest.Config) (*corev1client.CoreV1Client, error) {
+	client, err := corev1client.NewForConfig(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return client, nil
 }
 
-func GetRouteV1Client(fromLocal bool) (*routev1client.RouteV1Client, error) {
-	restConfig, err := GetConfig(fromLocal)
-	if err != nil {
-		return nil, err
-	}
-	client, err := routev1client.NewForConfig(restConfig)
+func GetRouteV1Client(restConf *rest.Config) (*routev1client.RouteV1Client, error) {
+	client, err := routev1client.NewForConfig(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return client, nil
 }
 
-func GetRbacV1Client(fromLocal bool) (*rbacv1client.RbacV1Client, error) {
-	restConfig, err := GetConfig(fromLocal)
-	if err != nil {
-		return nil, err
-	}
-	client, err := rbacv1client.NewForConfig(restConfig)
+func GetRbacV1Client(restConf *rest.Config) (*rbacv1client.RbacV1Client, error) {
+	client, err := rbacv1client.NewForConfig(restConf)
 	if err != nil {
 		return nil, err
 	}
 	return client, nil
 }
 
-func GetConfig(fromLocal bool) (*rest.Config, error) {
-	if fromLocal {
+const NamespaceEnvName = "NAMESPACE"
+
+func GetNamespace(fromEnv bool) string {
+	if fromEnv {
+		namespace := os.Getenv(NamespaceEnvName)
+		if namespace == "" {
+			panic("NAMESPACE was not set")
+		}
+		return namespace
+	} else {
+		kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+			clientcmd.NewDefaultClientConfigLoadingRules(),
+			&clientcmd.ConfigOverrides{},
+		)
+
+		namespace, _, err := kubeconfig.Namespace()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		if namespace == "" {
+			panic("NAMESPACE was not set")
+		}
+		return namespace
+	}
+}
+
+func GetRestConfig(inCluster bool) (*rest.Config, error) {
+	if inCluster {
+		// Build a rest.Config from configuration injected into the Pod by
+		// Kubernetes.  Clients will use the Pod's ServiceAccount principal.
+		restconfig, err := rest.InClusterConfig()
+		if err != nil {
+			return nil, err
+		}
+		return restconfig, nil
+	} else {
 		// Instantiate loader for kubeconfig file.
 		kubeconfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
 			clientcmd.NewDefaultClientConfigLoadingRules(),
@@ -68,15 +88,6 @@ func GetConfig(fromLocal bool) (*rest.Config, error) {
 		if err != nil {
 			return nil, err
 		}
-
 		return restConfig, nil
-	} else {
-		// Build a rest.Config from configuration injected into the Pod by
-		// Kubernetes.  Clients will use the Pod's ServiceAccount principal.
-		restconfig, err := rest.InClusterConfig()
-		if err != nil {
-			return nil, err
-		}
-		return restconfig, nil
 	}
 }
