d8a78c8dce161ec63c609c2b0c8659ab9cf6f2fc
delai21 <k.gysin.kg@gmail.com>
2020-04-14 20:57:15 +0200

    fix label

--- application/get.go
+++ application/get.go
@@ -6,17 +6,20 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-func NewAppFromNamespace(appName string, namespace string, restConf *rest.Config, options v12.ListOptions) (*Application, error) {
+func NewAppFromNamespace(appName string, namespace string, restConf *rest.Config, labelSelector string) (*Application, error) {
 	var items []appitem.AppItem
 	// Add All
-	newItems, err := appitem.ListAll(namespace, restConf, options)
+	newItems, err := appitem.ListAll(namespace, restConf, v12.ListOptions{
+		LabelSelector: labelSelector,
+		Limit:         0,
+	})
 	if err != nil {
 		return nil, err
 	}
 	items = append(items, newItems...)
 
 	// Create App
-	newOp := NewApp(appName)
+	newOp := NewAppWithLabel(appName, labelSelector)
 	newOp.Items = items
 	return newOp, nil
 }
